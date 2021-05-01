befc33b879383102ef4f880944fc5933f734b223
vijaya731 <65881300+vijaya731@users.noreply.github.com>
2021-05-01 22:01:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	 fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+star.velocityY=1
+//Matter.Body.setStatic(fairy> false);
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+	
+	star.x=starBody.position.x
+	star.y=starBody.position.y
+}
+
+
+function draw() {
+  background(bgImg);
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+	//write code here
+	if(keyDown(LEFT)){
+fairy.velocityX=-2
+
+	}
+	if(keyDown(RIGHT)){
+		fairy.velocityX=2
+		
+			}
+			if(star.isTouching(fairy)){
+				star.velocityY=0
+				fairy.velocityX=0
+			}
+}
